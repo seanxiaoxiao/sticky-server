eee611c46cadfd52b8ce409eb86b2018d123d3bc
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-06-15 18:32:27 -0700

    Solve problems

--- routes/index.js
+++ routes/index.js
@@ -1,71 +1,76 @@
 var Evernote = require('evernote').Evernote;
+var fs = require('fs');
+var crypto = require('crypto');
 
 var config = require('../config.json');
 var callbackUrl = "http://localhost:3000/oauth_callback";
 
+
+
 // home page
 exports.post_note = function(req, res) {
-  var title = req.title;
-  var coupon = req.body.coupon;
-  var deadline = req.deadline;
-  var authToken = "908d41ca2cad4374";
-  var client = new Evernote.Client({token: authToken, sandbox: true});
-  var userStore = client.getUserStore();
-  var noteStore = client.getNoteStore();
-  fs = require('fs');
-  crypto = require('crypto');
-  Evernote = require('evernote').Evernote;
+
+    if (req.session.oauthAccessToken) {
+        var client = new Evernote.Client({
+            consumerKey: config.API_CONSUMER_KEY,
+            consumerSecret: config.API_CONSUMER_SECRET,
+            sandbox: config.SANDBOX,
+            token: req.session.oauthAccessToken
+        });
+
+        var userStore = client.getUserStore();
+        var noteStore = client.getNoteStore();
+        var notebooks = noteStore.listNotebooks(function(notebooks) {
+            console.log("Found " + notebooks.length + " notebooks:");
+            for (var i in notebooks) {
+                console.log("  * " + notebooks[i].name);
+            }
+        });
+    }
+    res.end();
+
+
 // List all of the notebooks in the user's account
-var notebooks = noteStore.listNotebooks(function(notebooks) {
-  console.log("Found " + notebooks.length + " notebooks:");
-  for (var i in notebooks) {
-    console.log("  * " + notebooks[i].name);
-  }
-});
 
-// To create a new note, simply create a new Note object and fill in
-// attributes such as the note's title.
-var note = new Evernote.Note();
-note.title = "Test note from EDAMTest.js";
-
-// To include an attachment such as an image in a note, first create a Resource
-// for the attachment. At a minimum, the Resource contains the binary attachment
-// data, an MD5 hash of the binary data, and the attachment MIME type.
-// It can also include attributes such as filename and location.
-var image = fs.readFileSync('enlogo.png');
-var hash = image.toString('base64');
-
-var data = new Evernote.Data();
-data.size = image.length;
-data.bodyHash = hash;
-data.body = image;
-
-resource = new Evernote.Resource();
-resource.mime = 'image/png';
-resource.data = data;
-
-// Now, add the new Resource to the note's list of resources
-note.resources = [resource];
-
-// To display the Resource as part of the note's content, include an <en-media>
-// tag in the note's ENML content. The en-media tag identifies the corresponding
-// Resource using the MD5 hash.
-var md5 = crypto.createHash('md5');
-md5.update(image);
-hashHex = md5.digest('hex');
-
-// The content of an Evernote note is represented using Evernote Markup Language
-// (ENML). The full ENML specification can be found in the Evernote API Overview
-// at http://dev.evernote.com/documentation/cloud/chapters/ENML.php
-note.content = '<?xml version="1.0" encoding="UTF-8"?>';
-note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
-note.content += '<en-note>Here is the Evernote logo:<br/>';
-note.content += '<en-media type="image/png" hash="' + hashHex + '"/>';
-note.content += '</en-note>';
-
-// Finally, send the new note to Evernote using the createNote method
-// The new Note object that is returned will contain server-generated
-// attributes such as the new note's unique GUID.
+    // To create a new note, simply create a new Note object and fill in
+    // attributes such as the note's title.
+//    var note = new Evernote.Note();
+//    note.title = "Test note from EDAMTest.js";
+//
+//    // To include an attachment such as an image in a note, first create a Resource
+//    // for the attachment. At a minimum, the Resource contains the binary attachment
+//    // data, an MD5 hash of the binary data, and the attachment MIME type.
+//    // It can also include attributes such as filename and location.
+//    var image = fs.readFileSync('enlogo.png');
+//    var hash = image.toString('base64');
+//
+//    var data = new Evernote.Data();
+//    data.size = image.length;
+//    data.bodyHash = hash;
+//    data.body = image;
+//
+//    resource = new Evernote.Resource();
+//    resource.mime = 'image/png';
+//    resource.data = data;
+//
+//    // Now, add the new Resource to the note's list of resources
+//    note.resources = [resource];
+//
+//    // To display the Resource as part of the note's content, include an <en-media>
+//    // tag in the note's ENML content. The en-media tag identifies the corresponding
+//    // Resource using the MD5 hash.
+//    var md5 = crypto.createHash('md5');
+//    md5.update(image);
+//    hashHex = md5.digest('hex');
+//
+//    // The content of an Evernote note is represented using Evernote Markup Language
+//    // (ENML). The full ENML specification can be found in the Evernote API Overview
+//    // at http://dev.evernote.com/documentation/cloud/chapters/ENML.php
+//    note.content = '<?xml version="1.0" encoding="UTF-8"?>';
+//    note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">';
+//    note.content += '<en-note>Here is the Evernote logo:<br/>';
+//    note.content += '<en-media type="image/png" hash="' + hashHex + '"/>';
+//    note.content += '</en-note>';
 
 //noteStore.createNote(note, function(createdNote) {
 //  console.log();
@@ -95,11 +100,12 @@ exports.index = function(req, res) {
 
 // OAuth
 exports.oauth = function(req, res) {
-  var client = new Evernote.Client({
-    consumerKey: config.API_CONSUMER_KEY,
-    consumerSecret: config.API_CONSUMER_SECRET,
-    sandbox: config.SANDBOX
-  });
+   var client = new Evernote.Client({
+      consumerKey: config.API_CONSUMER_KEY,
+      consumerSecret: config.API_CONSUMER_SECRET,
+      sandbox: config.SANDBOX
+   });
+
 
   client.getRequestToken(callbackUrl, function(error, oauthToken, oauthTokenSecret, results){
     if(error) {
@@ -120,11 +126,11 @@ exports.oauth = function(req, res) {
 
 // OAuth callback
 exports.oauth_callback = function(req, res) {
-  var client = new Evernote.Client({
-    consumerKey: config.API_CONSUMER_KEY,
-    consumerSecret: config.API_CONSUMER_SECRET,
-    sandbox: config.SANDBOX
-  });
+    var client = new Evernote.Client({
+        consumerKey: config.API_CONSUMER_KEY,
+        consumerSecret: config.API_CONSUMER_SECRET,
+        sandbox: config.SANDBOX
+    });
 
   client.getAccessToken(
     req.session.oauthToken, 
@@ -144,6 +150,7 @@ exports.oauth_callback = function(req, res) {
         req.session.edamExpires = results.edam_expires;
         req.session.edamNoteStoreUrl = results.edam_noteStoreUrl;
         req.session.edamWebApiUrlPrefix = results.edam_webApiUrlPrefix;
+        req.session.client = client;
         res.redirect('/');
       }
     });
